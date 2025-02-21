db291ec2efc28d73ef51bdb704dd1b6dc9aa1364
Amir Kavoosi <amir4x2005@yahoo.com>
2025-02-21 11:20:12 -0500

    Updated main.ts and app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateFeatureComponent } from './create-feature/create-feature.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: 'create-feature', component: CreateFeatureComponent },
+
+];
